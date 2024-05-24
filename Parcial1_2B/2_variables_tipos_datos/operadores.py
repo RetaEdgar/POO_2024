637910213e1bd188b527d2d730f8dcc07cd4d1f3
RetaEdgar <edgarreta39@gmail.com>
2024-05-23 22:26:20 -0600

    Actualizacion 23/05

--- Parcial1_2B/2_variables_tipos_datos/operadores.py
+++ Parcial1_2B/2_variables_tipos_datos/operadores.py
@@ -1,3 +1,65 @@
 #Operadores en Python 
 
-#Aritmeticos@
+#Aritmeticos
+
+#Suma
+#x + y
+num1=float(input("Coloca el primer numero: "))
+num2=float(input("Coloca el segundo numero: "))
+
+print(f"El valor de la suma es: {num1 + num2}")
+
+#Resta
+#x - y
+print(f"El valor de la suma es: {num1 - num2}")
+
+#multiplicacion
+#x * y
+print(f"El valor de la suma es: {num1 * num2}")
+
+#Division
+#x / y
+print(f"El valor de la suma es: {num1 / num2}")
+
+#modulus (mood)
+# x % y
+print(f"El valor de la suma es: {num1 % num2}")
+
+#Exponente (elevacion)
+# x ** y
+print(f"El valor de la potencia del primer numero es: {num1 **2}")
+print(f"El valor de la potencia del segundo numero es: {num2 **2}")
+
+#floor division (Div)
+#x // y
+print(f"El valor de la suma es: {num1 // num2}")
+
+#Operadores de comparacion
+
+#Igual
+#x == y
+print(f"El valor de la suma es: {num1 == num2}")
+
+#Diferente
+#x != y
+print(f"El valor de la suma es: {num1 != num2}")
+
+#Mayor que
+#x > y
+print(f"El valor de la suma es: {num1 > num2}")
+
+#Menor que
+#x < y
+print(f"El valor de la suma es: {num1 < num2}")
+
+#Mayo o igual que
+#x >= y
+print(f"El valor de la suma es: {num1 >= num2}")
+
+#Menor o igual que
+#x <= y
+print(f"El valor de la suma es: {num1 <= num2}")
+
+
+
+
