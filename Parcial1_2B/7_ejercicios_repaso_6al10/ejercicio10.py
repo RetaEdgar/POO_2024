f5efaa2d635e596bc6f007edf828e3cf55296116
RetaEdgar <edgarreta39@gmail.com>
2024-05-23 19:49:18 -0600

    actualizar

--- Parcial1_2B/7_ejercicios_repaso_6al10/ejercicio10.py
+++ Parcial1_2B/7_ejercicios_repaso_6al10/ejercicio10.py
@@ -5,8 +5,8 @@ alu=1
 for i in range (1, 16):
     cali=float(input(f"Coloca la calificacion de tu alumno No: {alu}.- es:"))
     alu+=1
-    if cali >=6:
-        aprobado+=1
+    if cali >=8:
+        aprobado+=1 #hola 
     else:
         reprobado +=1
 
